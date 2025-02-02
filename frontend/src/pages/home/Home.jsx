deb6ab53e7f5bdd6edc4eb3cb7d15f402c18c19d
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-02 23:24:58 +0530

    Header coponenrs is completed succesfully

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/header/Header";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
