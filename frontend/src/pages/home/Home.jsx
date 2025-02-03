9a8bca78209037037f17b58d583e3dc0ae9bfaae
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-03 13:37:44 +0530

    componets which needs to shown in Homepage is almost done

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/featured";
+import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperty from "../../components/featuredProperties/FeaturedProperty";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property</h1>
+        <PropertyList />
+        <h1 className="homeTitle">homes guests love</h1>
+        <FeaturedProperty />
+      </div>
     </div>
   );
 };
--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/header/Header";
 const List = () => {
-  return <div>Hotels</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
 };
 
 export default List;
