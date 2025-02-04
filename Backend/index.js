346369010b0864a33514ea13501d933d3ec0652d
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-04 18:39:57 +0530

    Backend setup for project and simple routes created and mongoAtlas connection

---
+++ Backend/index.js
@@ -0,0 +1,42 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
+const app=express();
+dotenv.config()
+
+// const connect=async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO);
+//         console.log("connected to mogoDB")
+    
+//     }catch (error){
+//         throw error;
+
+//     }
+//     mongoose.connection.on("disconneted",()=>{
+//         console.log("MongoDb disconnected")
+//     })
+
+    
+
+// }
+
+app.get("/user",(req,res)=>{
+    res.send("hello first request");
+})
+
+// middlewere 
+app.use("/api/auth",authRoute)
+app.use("/api/users",authRoute)
+app.use("/api/hotels",authRoute)
+app.use("/api/rooms",authRoute)
+
+app.listen(8800,()=>{
+    // connect()
+    console.log("Connected to backend.");
+})
