df878a72837b9122355b6f4dc5ec211985f6e091
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-05 19:06:35 +0530

    Today i have done Nodejs Crud opreation and testing in postman

--- Backend/index.js
+++ Backend/index.js
@@ -31,10 +31,11 @@ app.get("/user",(req,res)=>{
 })
 
 // middlewere 
+app.use(express.json())
 app.use("/api/auth",authRoute)
-app.use("/api/users",authRoute)
-app.use("/api/hotels",authRoute)
-app.use("/api/rooms",authRoute)
+app.use("/api/users",usersRoute)
+app.use("/api/hotels",hotelsRoute)
+app.use("/api/rooms",roomsRoute)
 
 app.listen(8800,()=>{
     // connect()
--- Backend/routes/hotels.js
+++ Backend/routes/hotels.js
@@ -1,9 +1,70 @@
-import express from "express"
-
+import express, { Router } from "express"
+import Hotel from "../models/Hotel.model.js"
 const router=express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("Hello, this is hotles end point")
+// Create 
+router.post("/",async (req,res)=>{
+    const newHotel=new Hotel(req.body)
+    try {
+      const savedHotel=await newHotel.save()
+      res.status(200).json(savedHotel)
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
 })
+
+// UPDATE
+router.put("/:id",async (req,res)=>{
+ 
+  try {
+    const updatedHotel=await Hotel.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+    res.status(200).json( updatedHotel)
+      
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
+})
+
+// DELETE
+router.delete("/:id",async (req,res)=>{
+ 
+  try {
+    await Hotel.findByIdAndDelete(req.params.id)
+    res.status(200).json("Hotel hase been deleted.")
+      
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
+})
+// GET
+router.get("/:id",async (req,res)=>{
+ 
+  try {
+    const hotel=await Hotel.findById(req.params.id)
+    res.status(200).json(hotel)
+      
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
+})
+// GETALL 
+router.get("/:id",async (req,res)=>{
+ 
+  try {
+    const hotels=await Hotel.find(req.params.id)
+    res.status(200).json(hotels)
+      
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  
+})
+
+
 
 export default router
