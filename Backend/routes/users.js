61aedcf20deca7cbb97768e5b5c62e38f74e78e4
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-07 18:46:04 +0530

    CRUD opeartion for User

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,16 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
 
 const router=express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("Hello, this is users end point")
-})
+// UPDATE
+router.put("/:id",updateUser)
+
+// DELETE
+router.delete("/:id",deleteUser)
+// GET
+router.get("/:id",getUser)
+// GETALL 
+router.get("/:id",getUsers)
 
 export default router
