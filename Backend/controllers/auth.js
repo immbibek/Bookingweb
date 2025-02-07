012c4f89d1ba76433b2a13eb26418e66451dc015
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-07 18:02:27 +0530

    login backend logic is written

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/Users.model.js"
 import bcrypt from "bcryptjs"
+
 export const register=async (req,req,next)=>{
     try {
         const salt=bcrypt.genSaltSync(10)
@@ -16,6 +17,27 @@ export const register=async (req,req,next)=>{
         await newUser.save()
         res.status(200).send("User created")
         
+    } catch (err) {
+        next(err)
+        
+    }
+
+}
+
+
+export const login=async (req,req,next)=>{
+    try {
+       const user=await User.findOne({username:req.body.username})
+
+       if(!user) return next(createError(404,"User not found"))
+
+        const  isPasswordCorrect=await bcrypt.compare(req.body.password,user.password)
+
+        if(!isPasswordCorrect) return next(createError(400,"Wrong password or username"))
+
+       const {password,isAdmin,...otherDetalis}=user._doc;
+
+        res.status(200).json(...otherDetalis)
     } catch (err) {
         next(err)
         
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,4 +4,7 @@ const router=express.Router();
 
 router.post("/register",register)
 
+router.post("/login",login)
+
+
 export default router
