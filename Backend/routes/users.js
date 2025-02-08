8248791ad0f09dcc04c27d06a11edb45eb4abedd
Bibek <137331749+immbibek@users.noreply.github.com>
2025-02-08 21:00:26 +0530

    Role based authentication

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,16 +1,29 @@
 import express from "express"
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken,verifyUser ,verifyAdmin} from "../utils/verifyToken.js";
 
 const router=express.Router();
 
+// router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+//     res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+//     res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id",verifyUser,(req,res,next)=>{
+//     res.send("hello Admin, you are logged in and you can delete all account")
+// })
+
 // UPDATE
-router.put("/:id",updateUser)
+router.put("/:id",verifyUser,updateUser)
 
 // DELETE
-router.delete("/:id",deleteUser)
+router.delete("/:id",verifyUser,deleteUser)
 // GET
-router.get("/:id",getUser)
+router.get("/:id",verifyUser,getUser)
 // GETALL 
-router.get("/:id",getUsers)
+router.get("/:id",verifyAdmin,getUsers)
 
 export default router
